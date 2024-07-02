0a39de7b7a480354a164b5f66ef44cebe08910ae
devK7 <yourfunnybrother@outlook.com>
2024-07-02 20:47:28 +0800

    文章id2出现

--- js/view.js
+++ js/view.js
@@ -1,14 +1,19 @@
-$(function(){
+$(async function(){
     get_post_data();
 });
-function get_post_data() {
+async function get_post_data() {
     const view_id = window.location.pathname.trim().replace(/^\/+|\/+$/g, "").split("/").filter(segment => segment !== "")[window.location.pathname.trim().replace(/^\/+|\/+$/g, "").split("/").filter(segment => segment !== "").length-1];
-    $.getJSON(`/db/post/data/${view_id}.json`, function(data) {
+    $.getJSON(`/db/post/data/${view_id}.json`, async function(data) {
         const p_meta = data["meta"]
         const p_view = new pv(p_meta["id"], p_meta["title"], p_meta["description"], p_meta["time"], p_meta["author"], p_meta["visitable"], p_meta["tags"]);
         $("#v_metadata").html(p_view.html);
         $("title").html(p_meta["visitable"] ? p_meta["title"] : "错误: 权限不足");
-        if (!p_meta["visitable"]) throw "文章不可查看, 原因: 权限不足";
+        if (!p_meta["visitable"]) {
+            $("#v_content").remove();
+            await new Promise(r => setTimeout(r, (500)));
+            alert(`权限不足, json中权限标记为\n"visitable": ${p_meta["visitable"]}`);
+            throw "文章不可查看, 原因: 权限不足";
+        }
     });
     $.get(`/db/post/data/${view_id}.md`, function(data) {
         $("#v_content").html(marked.parse(data.toString()));
