7dfcd68d8cfc69d76dfa884020674bbabaf5ff87
devK7 <yourfunnybrother@outlook.com>
2024-07-03 18:40:33 +0800

    更新了使用文档（该咋咋地）

--- js/docs.js
+++ js/docs.js
@@ -3,6 +3,11 @@ const docs_nav_arr = [
         display_name: "如何使用本站",
         name: "如何使用本站",
         ui: null
+    },
+    {
+        display_name: "如何写出正确的废话",
+        name: "如何写出正确的废话",
+        ui: null
     }
 ]
 $(async function() {
@@ -10,5 +15,7 @@ $(async function() {
     $.get("/db/docs/本站使用教程.md", async function(data) {
         $(".本站使用教程").html(marked.parse(data));
     });
-    
+    $.get("/db/docs/写出正确的废话教程.md", async function(data) {
+        $(".写出正确的废话教程").html(marked.parse(data));
+    });
 });
