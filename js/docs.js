a7edf4981d59cdbd38ce995291ff0e69b11d316b
devK7 <yourfunnybrother@outlook.com>
2024-07-05 18:43:38 +0800

    优化算法

--- js/docs.js
+++ js/docs.js
@@ -1,53 +1,14 @@
-const docs_nav_arr = [
-    {
-        display_name: "如何使用本站",
-        name: "如何使用本站",
-        ui: null
-    },
-    {
-        display_name: "如何写出正确的废话",
-        name: "如何写出正确的废话",
-        ui: null
-    },
-    {
-        display_name: "电脑如何开关机",
-        name: "电脑如何开关机",
-        ui: null
-    },
-    {
-        display_name: "如何下载软件",
-        name: "如何下载软件",
-        ui: null
-    },
-    {
-        display_name: "如何正确提问",
-        name: "如何正确提问",
-        ui: null
-    },
-    {
-        display_name: "如何提交项目更改",
-        name: "如何提交项目更改",
-        ui: null
-    }
-]
+const docs_arr = [
+    "本站使用教程",
+    "写出正确的废话教程",
+    "电脑开关机教程",
+    "如何下载软件",
+    "如何正确提问",
+    "如何提交项目更改"
+];
+var docs_nav_arr = [];
 $(async function() {
+    docs_arr.forEach((item, index) => docs_nav_arr.push({display_name:item, name: item, ui: null}));
     var nav = new Navigation.Navigation(docs_nav_arr, "docs");
-    $.get("/db/docs/本站使用教程.md", async function(data) {
-        $(".本站使用教程").html(marked.parse(data));
-    });
-    $.get("/db/docs/写出正确的废话教程.md", async function(data) {
-        $(".写出正确的废话教程").html(marked.parse(data));
-    });
-    $.get("/db/docs/电脑开关机教程.md", async function(data) {
-        $(".电脑开关机教程").html(marked.parse(data));
-    });
-    $.get("/db/docs/如何下载软件.md", async function(data) {
-        $(".如何下载软件").html(marked.parse(data));
-    });
-    $.get("/db/docs/如何正确提问.md", async function(data) {
-        $(".如何正确提问").html(marked.parse(data));
-    });
-    $.get("/db/docs/如何提交项目更改.md", async function(data) {
-        $(".如何提交项目更改").html(marked.parse(data));
-    });
+    docs_arr.forEach((item, index) => $.get(`/db/docs/${item}.md`, (data) => $(`.${item}`).html(marked.parse(data))));
 });
